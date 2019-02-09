5c5a3209c3141e2513bd9d6be7d971e1084a654a
RubenJromo <rjromo@espol.edu.ec>
2019-02-08 20:52:08 -0500

    1st Commit

---
+++ src/Lesson 3 Quiz 6.kt
@@ -0,0 +1,11 @@
+package demo
+
+fun main(args: Array<String>){
+    val spices = listOf("curry", "pepper", "cayenne", "ginger", "red curry", "green curry", "red pepper" )
+
+    val curries = spices.filter { it.contains("curry") }.sortedBy { it.length }
+
+    val startswithc = spices.filter { {it.startsWith('c') && it.endsWith('e') }
+
+    val 3itemswithc = spices.take(3).filter{it.startsWith('c')}
+}
